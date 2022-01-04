4c6b2920153b863ffad3c623c5b5b849ae1896eb
Anderson Dolce <dolce436@gmail.com>
2022-01-04 15:24:33 -0300

    "projecao dados author com dto2"

--- src/main/java/com/andersondolce/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/andersondolce/workshopmongo/dto/AuthorDTO.java
@@ -23,4 +23,20 @@ public class AuthorDTO implements Serializable {
         this.id = id;
         this.name = name;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
