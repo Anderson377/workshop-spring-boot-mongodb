120fcd7b31bd2ec5ad76b95124df4eb4dc0fca30
Anderson Dolce <dolce436@gmail.com>
2022-01-05 10:38:28 -0300

    "Teste fullSearch"

--- src/main/java/com/andersondolce/workshopmongo/services/PostService.java
+++ src/main/java/com/andersondolce/workshopmongo/services/PostService.java
@@ -1,14 +1,12 @@
 package com.andersondolce.workshopmongo.services;
 
 import com.andersondolce.workshopmongo.domain.Post;
-import com.andersondolce.workshopmongo.domain.User;
-import com.andersondolce.workshopmongo.dto.UserDTO;
 import com.andersondolce.workshopmongo.repository.PostRepository;
-import com.andersondolce.workshopmongo.repository.UserRepository;
 import com.andersondolce.workshopmongo.services.exception.ObjetoNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +22,11 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 
+    public List<Post> fullSearch(String text, String minDate, String maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
 }
